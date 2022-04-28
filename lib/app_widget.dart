ba6f20c51091c8c5109128496436e51a13f84820
Janielly Aliny <janiellyaliny@air-de-janielly.lan>
2022-04-28 16:36:23 -0300

    feat: configuration for startup

---
+++ lib/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (_) => MaterialApp.router(
+        title: 'View Movie',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routerDelegate: Modular.routerDelegate,
+        routeInformationParser: Modular.routeInformationParser,
+      ),
+    );
+  }
+}
