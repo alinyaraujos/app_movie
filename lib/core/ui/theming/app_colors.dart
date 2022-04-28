914b61d0c352f41c84867488e7c525a4acd9b1f4
Janielly Aliny <janiellyaliny@air-de-janielly.lan>
2022-04-28 16:29:43 -0300

    feat: theme settings

---
+++ lib/core/ui/theming/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFF5CA0D3);
+  static const Color blue = Color(0xFF2c3848);
+  static const Color yellow= Color(0xFFFCD307);
+}
