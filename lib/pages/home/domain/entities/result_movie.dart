692acd812af5d33eeb1c22e665631c78ea8a5ce9
Janielly Aliny <janiellyaliny@air-de-janielly.lan>
2022-04-28 17:11:48 -0300

    feat: result movie entities

---
+++ lib/pages/home/domain/entities/result_movie.dart
@@ -0,0 +1,13 @@
+import 'package:app_movie/pages/home/domain/entities/movie.dart';
+
+class ResultMovie {
+  final int page;
+  final int totalPages;
+  final List<Movie> results;
+
+  ResultMovie({
+    required this.page,
+    required this.totalPages,
+    required this.results,
+  });
+}
