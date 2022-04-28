d3b2316ee907696cfb4cd8bd9f5e58c4ab55e1e0
Janielly Aliny <janiellyaliny@air-de-janielly.lan>
2022-04-28 17:10:57 -0300

    feat: movie entities

---
+++ lib/pages/home/domain/entities/movie.dart
@@ -0,0 +1,13 @@
+class Movie {
+  final String title;
+  final String posterPath;
+  final String overview;
+  final double voteAverage;
+
+  Movie({
+    required this.title,
+    required this.posterPath,
+    required this.overview,
+    required this.voteAverage,
+  });
+}
